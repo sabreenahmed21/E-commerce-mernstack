bd94823c4c0a79b246915598faeb3c623370632a
sabreenahmed21 <sabreenahmed212@gmail.com>
2024-03-24 17:59:06 +0200

    change render cors

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://bazaar-2tyg.onrender.com"],
     credentials: true,
   })
 );
